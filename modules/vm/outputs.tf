d80952983a3e9757741922199fab6c3e90fca724
dinodem <132165569+dinodem@users.noreply.github.com>
2025-02-14 17:18:46 +0100

    Update outputs.tf
    don't get the IP:s created in other states

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,3 +1,3 @@
 output "vm_ipv4_address" {
-  value = proxmox_virtual_environment_vm.vm.ipv4_addresses[1][0]
+  value = length(proxmox_virtual_environment_vm.vm.ipv4_addresses) > 0 ? proxmox_virtual_environment_vm.vm.ipv4_addresses[1][0] : "Only showing IP assigned in this tf state"
 }
